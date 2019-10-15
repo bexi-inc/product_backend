194a977bbaffa4b6db7d812d7ad4741bc84985f5
Manuel Cuevas <manuel@bexi.io>
2019-10-15 11:54:54 -0600

    Fix bug on module viewer

--- generator/view_module.php
+++ generator/view_module.php
@@ -58,36 +58,7 @@ $contenido="";
 $contenido=$marshaler->unmarshalValue($result['Items'][0]['html_code']);
 $css[]=$marshaler->unmarshalValue($result['Items'][0]["file_css"]);
 
-$pos = 0;
-$pos2 = 0;
-while ( ( $pos = strpos( $contenido, "%img", $pos ) ) !== false ) {
-  $pos2 = strpos( $contenido, "%", ($pos + 1) );
- // echo "The letter 'l' was found at position: $pos<br/>";
-  $imgtag = substr($contenido,$pos+1,$pos2-$pos - 1);
- // echo $imgtag;
-
-  $imgdata = explode ("|",$imgtag);
-  //print_r( $imgdata);
-
-    $filters = [
-            'featured' => true,
-            'w'        => $imgdata[1],
-            'h'        => $imgdata[2]
-    ];
-
-    //print_r($filters);
-    try
-    {
-        $data = Crew\Unsplash\Photo::random($filters);
-     
-        $contenido=substr_replace($contenido,$data->urls['custom'],$pos,$pos2-$pos);
-       
-    }catch (Crew\Unsplash\Exception $e) {
-          //writeErrorLogEntry(basename(__FILE__,'.php'),__LINE__,$e);
-    }
-     $pos++;
-    
-}
+$contenido= setImages($contenido,"");
 
 
 $pos = 0;
