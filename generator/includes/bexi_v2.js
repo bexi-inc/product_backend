be8faaacba84780c9f598419bd38f93fb0c132ef
Manuel Cuevas <manuel@bexi.io>
2019-10-15 06:07:34 -0600

    add text editor

--- generator/includes/bexi_v2.js
+++ generator/includes/bexi_v2.js
@@ -175,14 +175,16 @@
         $( ".bexi_title" ).wrap( "<div class='bexi_editor_title'></div>" );
 
         $( ".bexi_subtitle" ).wrap( "<div class='bexi_editor_subtitle'></div>" );
-/*
-        var editortxt = new FroalaEditor('.bexi_text',
+
+        $( ".bexi_text" ).wrap( "<div class='bexi_editor_text'></div>" );
+
+        var editortxt = new FroalaEditor('.bexi_editor_text',
         {
           key  :   "CTD5xE3F3E2B1A4A1wnhvfF1rH-7oA9A7B6E5C2H4E3J2A7B8==",
           toolbarInline: true,
           charCounterCount: false,
           initOnClick: true
-        });*/
+        });
 
         var editortitles = new FroalaEditor('.bexi_editor_title',
         {
