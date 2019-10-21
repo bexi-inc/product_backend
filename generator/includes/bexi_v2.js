77f205340b44ab9f4b875e2412f3b5674e2b4664
Manuel Cuevas <manuel@bexi.io>
2019-10-21 09:17:10 -0600

    Add Button Editor.

--- generator/includes/bexi_v2.js
+++ generator/includes/bexi_v2.js
@@ -180,6 +180,8 @@
 
          $( ".bexi_link" ).wrap( "<div class='bexi_editor_link'></div>" );
 
+          $( ".bexi_button" ).wrap( "<div class='bexi_editor_button'></div>" );
+
         
         var editortxt = new FroalaEditor('.bexi_editor_text',
         {
@@ -220,6 +222,16 @@
           charCounterCount: false,
           initOnClick: true,
           imageManagerLoadURL: 'load_images.php',
+          toolbarBottom : false
+
+        });
+
+         var editorbtn = new FroalaEditor('.bexi_editor_button',
+        {
+          key  :   "CTD5xE3F3E2B1A4A1wnhvfF1rH-7oA9A7B6E5C2H4E3J2A7B8==",
+          toolbarInline: true,
+          charCounterCount: false,
+          initOnClick: true,
           toolbarBottom : false
 
         });
