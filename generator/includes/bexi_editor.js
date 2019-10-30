c7d9986eaa5f60e8b337dd6fda1ae99f3b6c9c09
Adryx46 <adrian@bexi.io>
2019-10-30 16:20:31 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -128,9 +128,6 @@
          var editorlin = new FroalaEditor('.bexi_editor_link',
         {
           key  :   "CTD5xE3F3E2B1A4A1wnhvfF1rH-7oA9A7B6E5C2H4E3J2A7B8==",
-          toolbarInline: true,
-          charCounterCount: false,
-          initOnClick: true,
           linkButtons : {
             'moreText': {
                'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
