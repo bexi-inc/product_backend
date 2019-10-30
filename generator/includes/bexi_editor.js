2bb2f1c31c47fec8082b1d93f37a5f8adc89eda5
Adryx46 <adrian@bexi.io>
2019-10-30 16:17:19 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -131,7 +131,7 @@
           toolbarInline: true,
           charCounterCount: false,
           initOnClick: true,
-          toolbarButtons : {
+          linkButtons : {
             'moreText': {
                'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
              },
