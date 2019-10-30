efee0ef48f40ea301504db050ffa869e5d806d51
Adryx46 <adrian@bexi.io>
2019-10-30 16:20:51 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -171,7 +171,7 @@
           videoInsertButtons: ['videoBack', '|', 'videoByURL']
         });
 
-        var editorico = new FroalaEditor('.bexi_edito_icon', {
+        var editorico = new FroalaEditor('.bexi_editor_icon', {
           key  :   "CTD5xE3F3E2B1A4A1wnhvfF1rH-7oA9A7B6E5C2H4E3J2A7B8==",
           toolbarInline: true,
           charCounterCount: false,
