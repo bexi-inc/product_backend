6cb59b81d7c374a7f4d051d6e5b010073a630094
Adryx46 <adrian@bexi.io>
2019-10-30 15:36:17 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -163,5 +163,14 @@
           toolbarButtons: ['insertVideo'],
           videoAllowedProviders: ['youtube', 'vimeo'],
           videoInsertButtons: ['videoBack', '|', 'videoByURL']
-        })
+        });
+
+        var editorico = new FroalaEditor('.bexi_icon', {
+          key  :   "CTD5xE3F3E2B1A4A1wnhvfF1rH-7oA9A7B6E5C2H4E3J2A7B8==",
+          toolbarInline: true,
+          charCounterCount: false,
+          initOnClick: true,
+          toolbarBottom : false,
+          toolbarButtons: ['fontAwesome']
+        });
 });
