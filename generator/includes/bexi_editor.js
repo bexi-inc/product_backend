ada413f5f496b05b9d31b1931142791238f6dd6c
Adryx46 <adrian@bexi.io>
2019-10-30 16:39:08 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -130,7 +130,8 @@
           key  :   "CTD5xE3F3E2B1A4A1wnhvfF1rH-7oA9A7B6E5C2H4E3J2A7B8==",
           toolbarInline: true,
           charCounterCount: false,
-          initOnClick: true
+          initOnClick: true,
+          linkEditButtons:['linkOpen', 'linkStyle', 'linkEdit', 'linkRemove','bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
         });
 
          var editorimg = new FroalaEditor('.bexi_img',
@@ -172,6 +173,7 @@
           charCounterCount: false,
           initOnClick: true,
           toolbarBottom : false,
-          toolbarButtons: ['fontAwesome']
+          toolbarButtons: ['fontAwesome'],
+          faButtons: ["fontAwesomeBack", "|"]
         });
 });
