abd521648143a4923e1e5e4a620860ed92c45147
Adryx46 <adrian@bexi.io>
2019-10-30 17:58:10 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -174,7 +174,7 @@
           charCounterCount: false,
           initOnClick: true,
           toolbarBottom : false,
-          htmlAllowedEmptyTags: ['i'],
+          htmlAllowedEmptyTags: ['i','.fas'],
           toolbarButtons : {
             'moreText': {
                'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
