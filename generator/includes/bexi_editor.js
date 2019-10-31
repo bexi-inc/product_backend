269ba71fc756417f0c449e5f67622f9c60869004
Adryx46 <adrian@bexi.io>
2019-10-30 17:53:50 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -175,7 +175,20 @@
           initOnClick: true,
           toolbarBottom : false,
           htmlAllowedEmptyTags: ['i'],
-          toolbarButtons: ['fontAwesome'],
+          toolbarButtons : {
+            'moreText': {
+               'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
+             },
+             'moreParagraph': {
+               'buttons': ['alignLeft', 'alignCenter', 'formatOLSimple', 'alignRight', 'alignJustify', 'formatOL', 'formatUL', 'paragraphFormat', 'paragraphStyle', 'lineHeight', 'outdent', 'indent', 'quote']
+             },
+             'moreRich': {
+               'buttons': ['insertLink', 'insertImage', 'insertVideo', 'insertTable', 'emoticons', 'fontAwesome', 'specialCharacters', 'embedly', 'insertFile', 'insertHR']
+             },
+             'bexi_extra' : {
+                 'buttons' : ['ContentBlock']
+             }
+         },
           faButtons: ["fontAwesomeBack", "|"]
         });
 });
