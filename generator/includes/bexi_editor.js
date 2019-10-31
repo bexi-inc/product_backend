d43543717b0c47311e21f5e4edc652826159d505
Adryx46 <adrian@bexi.io>
2019-10-31 14:14:27 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -184,7 +184,6 @@
           toolbarBottom : false,
           toolbarVisibleWithoutSelection: true,
           htmlAllowedEmptyTags: ['i','.fas','div'],
-          fontAwesomeTemplate: '<i class="fas fa-[NAME] fr-deletable" aria-hidden="true">&nbsp;</i>',
           toolbarButtons : {
             'moreText': {
                'buttons': ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'fontFamily', 'fontSize', 'textColor', 'backgroundColor', 'inlineClass', 'inlineStyle', 'clearFormatting']
