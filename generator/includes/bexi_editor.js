8b3c5a6d16dd36afad8479e5c2755d6c6d494961
Adryx46 <adrian@bexi.io>
2019-11-01 14:48:30 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -24,7 +24,7 @@
             modal: true,
             buttons: {
               "Save": function() {
-                $($(this).attr("data-id")).closest(".bexi_module").attr('style','position:"relative"; background-color:'+$("#colorpicker_1").minicolors("rgbString")+'!important;');
+                $($(this).attr("data-id")).closest(".bexi_module").attr('style','position:relative; background-color:'+$("#colorpicker_1").minicolors("rgbString")+'!important;');
                 $( this ).dialog( "close" );
               },
               Cancel: function() {
