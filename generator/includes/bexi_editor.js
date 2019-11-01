b093975a92cd38740cd6762931e0de0e870ed5e8
Adryx46 <adrian@bexi.io>
2019-11-01 09:37:53 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -201,8 +201,7 @@
          }
         });
 
-        
-        var editorbtn = new FroalaEditor('.bexi_module',
+        var editormodule = new FroalaEditor('.bexi_module',
         {
           key  :   "CTD5xE3F3E2B1A4A1wnhvfF1rH-7oA9A7B6E5C2H4E3J2A7B8==",
           toolbarInline: true,
