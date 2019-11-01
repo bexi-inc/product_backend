a86807247237acea3b59cbecae4ce65b5d41eabd
Adryx46 <adrian@bexi.io>
2019-11-01 10:01:47 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -22,7 +22,7 @@
        $( ".bexi_video" ).wrap( "<div class='bexi_editor_video' style='width: 100%;'></div>" );
 
        $(".bexi_img").addClass("fr-view fr-dib");
-       $(".bexi_module").prepend($('<div class="bexi_module_bg transpa-bg" style="background-size: cover; position: absolute; top: 0; left: 0; width: 100%;height: 100%; z-index: -1;"></div>'));
+       $(".bexi_module").prepend($('<div class="bexi_module_bg transpa-bg" style="background-size: cover; position: absolute; top: 0; left: 0; width: 100%;height: 100%; z-index: 0;"></div>'));
        //$( ".bexi_icon" ).wrap( "<div class='bexi_editor_icon'></div>" );
 
         FroalaEditor.ICON_DEFAULT_TEMPLATE = "font_awesome_5";
