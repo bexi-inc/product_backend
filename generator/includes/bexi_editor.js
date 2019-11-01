6bfcbd9144f9e7c4315208a4ae6b91b8ae95cdcd
Adryx46 <adrian@bexi.io>
2019-11-01 11:57:38 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -63,7 +63,7 @@
                         //alert($(this).attr("data-id"));
                         //alert($("#colorpicker_1").minicolors("rgbString"));
                         //$($(this).attr("data-id")).closest(".bexi_module").css("background-color",$("#colorpicker_1").minicolors("rgbString"))
-                        $($(this).attr("data-id")).closest(".bexi_module").style.setProperty("background-color",$("#colorpicker_1").minicolors("rgbString"),'important')
+                        $($(this).attr("data-id")).closest(".bexi_module").attr('style', 'background-color:'+$("#colorpicker_1").minicolors("rgbString")+'!important');
                         $( this ).dialog( "close" );
 
                       },
