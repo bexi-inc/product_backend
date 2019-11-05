a99bf4c218986867ceb0a39e51ad9c4e34b62b3e
Adryx46 <adrian@bexi.io>
2019-11-05 10:24:04 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -78,6 +78,7 @@
       {
         var exist=true;
       }
+      alert(exist);
       if(exist==false){
         $('#'+ID).closest(".bexi_module").prepend('<div class="transpa-bg" style="background-image: url(""); background-size: cover; position: absolute; top: 0; left: 0; width: 100%;height: 100%; z-index: -1;"></div>');
       }
