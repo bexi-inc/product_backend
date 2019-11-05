bb4e512a5a6ecbb8de557077c80ac91113b05954
Adryx46 <adrian@bexi.io>
2019-11-05 14:03:49 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -14,8 +14,7 @@ function validate_url(url){
 
 function bgchangeurl(ID)
 {
-  var url=$("inptext"+ID).val();
-  alert(ID);
+  var url=$("#inptext"+ID).val();
   alert(url);
   if(url!=""){
     if(validate_url(url)==true)
