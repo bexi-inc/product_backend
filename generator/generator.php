92684ab4e6d3e8ec998835493ad25cbcbeccc2f3
Manuel Cuevas <manuel@bexi.io>
2019-11-05 08:57:38 -0700

    Fix Database Writers

--- generator/generator.php
+++ generator/generator.php
@@ -59,7 +59,7 @@ if (isset($_REQUEST["user"]) && isset($_REQUEST["codeid"]))
 
      $result = $dynamodb->query($params);
 
-     echo gzuncompress(base64_decode($marshaler->unmarshalValue($result['Items'][0]["code"])));
+     $contenido =  gzuncompress(base64_decode($marshaler->unmarshalValue($result['Items'][0]["code"])));
 }
 else{
 
@@ -260,6 +260,7 @@ else{
       $idrand = uniqid('bexi_');
       $contenido=substr_replace($contenido,$idrand,$pos,4);
     }
+}
 
     $iduser = $_REQUEST["user"];
     $CodeId = date("U");
@@ -352,82 +353,6 @@ else{
     //echo "</div>";
 
     echo "\r\n";
-
-    echo ' <script>
-                anima_isHidden = function(e) {
-                    if (!(e instanceof HTMLElement)) return !1;
-                    if (getComputedStyle(e).display == "none") return !0; else if (e.parentNode && anima_isHidden(e.parentNode)) return !0;
-                    return !1;
-                };
-                anima_loadAsyncSrcForTag = function(tag) {
-                    var elements = document.getElementsByTagName(tag);
-                    var toLoad = [];
-                    for (var i = 0; i < elements.length; i++) {
-                        var e = elements[i];
-                        var src = e.getAttribute("src");
-                        var loaded = (src != undefined && src.length > 0 && src != "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
-                        if (loaded) continue;
-                        var asyncSrc = e.getAttribute("anima-src");
-                        if (asyncSrc == undefined || asyncSrc.length == 0) continue;
-                        if (anima_isHidden(e)) continue;
-                        toLoad.push(e);
-                    }
-                    toLoad.sort(function(a, b) {
-                        return anima_getTop(a) - anima_getTop(b);
-                    });
-                    for (var i = 0; i < toLoad.length; i++) {
-                        var e = toLoad[i];
-                        var asyncSrc = e.getAttribute("anima-src");
-                        e.setAttribute("src", asyncSrc);
-                    }
-                };
-                anima_pauseHiddenVideos = function(tag) {
-                    var elements = document.getElementsByTagName("video");
-                    for (var i = 0; i < elements.length; i++) {
-                        var e = elements[i];
-                        var isPlaying = !!(e.currentTime > 0 && !e.paused && !e.ended && e.readyState > 2);
-                        var isHidden = anima_isHidden(e);
-                        if (!isPlaying && !isHidden && e.getAttribute("autoplay") == "autoplay") {
-                            e.play();
-                        } else if (isPlaying && isHidden) {
-                            e.pause();
-                        }
-                    }
-                };
-                anima_loadAsyncSrc = function(tag) {
-                    anima_loadAsyncSrcForTag("img");
-                    anima_loadAsyncSrcForTag("iframe");
-                    anima_loadAsyncSrcForTag("video");
-                    anima_pauseHiddenVideos();
-                };
-                var anima_getTop = function(e) {
-                    var top = 0;
-                    do {
-                        top += e.offsetTop || 0;
-                        e = e.offsetParent;
-                    } while (e);
-                    return top;
-                };
-                anima_loadAsyncSrc();
-                anima_old_onResize = window.onresize;
-                anima_new_onResize = undefined;
-                anima_updateOnResize = function() {
-                    if (anima_new_onResize == undefined || window.onresize != anima_new_onResize) {
-                        anima_new_onResize = function(x) {
-                            if (anima_old_onResize != undefined) anima_old_onResize(x);
-                            anima_loadAsyncSrc();
-                        };
-                        window.onresize = anima_new_onResize;
-                        setTimeout(function() {
-                            anima_updateOnResize();
-                        }, 3000);
-                    }
-                };
-                anima_updateOnResize();
-                setTimeout(function() {
-                    anima_loadAsyncSrc();
-                }, 200);
-            </script>';
     echo "\r\n";
     echo "</body>";
     echo "\r\n";
@@ -465,5 +390,5 @@ else{
         }
     }
     echo $code;
-}
+
 ?>
