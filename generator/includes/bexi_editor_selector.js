86976b65743fc4c5802aeeb918050b38e20c5f4a
Manuel Cuevas <manuel@bexi.io>
2019-11-05 17:33:52 -0700

    Fix Bug

--- generator/includes/bexi_editor_selector.js
+++ generator/includes/bexi_editor_selector.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   console.log("iniciando");
   $("#img_select_project").click(function(){
   		console.log("Project Selected");
-  		parent.SelectProject($(this).attr("bexi-code"));
+  		window.top.SelectProject($(this).attr("bexi-code"));
   });
 });
