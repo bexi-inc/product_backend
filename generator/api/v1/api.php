122a78420e2ee9d4a5287e56420a8b8727b8766f
Manuel Cuevas <manuel@bexi.io>
2019-11-11 18:30:20 -0700

    Fix Bug

--- generator/api/v1/api.php
+++ generator/api/v1/api.php
@@ -93,6 +93,7 @@ $res["error_code"]=0;
  		$res= CreateBucket($_REQ->subdomian);
  		break;
  	case 'SendEmail':
+ 		print_r($_REQ);
  		if (!isset($_REQ->type) || !isset($_REQ->user))
  		{
  			$res["error_code"]="502";
