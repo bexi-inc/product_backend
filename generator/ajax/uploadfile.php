ffa0560ea1ab12ce65285e999f47c14643cbd167
Manuel Cuevas <manuel@bexi.io>
2019-11-13 20:24:28 -0700

    fix bug

--- generator/ajax/uploadfile.php
+++ generator/ajax/uploadfile.php
@@ -29,10 +29,10 @@ $target_file = $fullpath . basename($_FILES["tmp_update"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-if (move_uploaded_file($_FILES["tmp_update"], $fullpath)) {
+if (move_uploaded_file($_FILES["tmp_update"], $target_file)) {
 
 }
 
-print_r($fullpath);
+print_r($target_file);
 
 ?>
