63a9ab67b4b873c0cd12b45b69f5a0c7c12566d7
Adryx46 <adrian@bexi.io>
2019-11-14 09:13:04 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -494,10 +494,9 @@ function bgchange(btid) {
         var response =save_img(id,$('#inpimg'+ID).prop('files')[0]);
         response.done(function(data){
           var jdata=JSON.parse(data);
-          $('#collapsetools'+ID).closest(".bexi_module").find(".transpa-bg").css("background-image","url("+data.src+")");
-          $('#collapsetools'+ID).closest(".bexi_module").find(".transpa-bg").attr("id",data.id);
+          $('#collapsetools'+ID).closest(".bexi_module").find(".transpa-bg").css("background-image","url("+jdata.src+")");
+          $('#collapsetools'+ID).closest(".bexi_module").find(".transpa-bg").attr("id",jdata.id);
         });
-        console.log(response);
         /*
               var jresponse = JSON.parse(response);
       $(OBJECT).css("background-image","url("+jresponse.src+")");
