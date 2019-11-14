3c87cad34a4b6a3a28d43f88ec0c9d9e3cf2c8c3
Manuel Cuevas <manuel@bexi.io>
2019-11-13 18:28:35 -0700

    Fix Error

--- generator/ajax/uploadfile.php
+++ generator/ajax/uploadfile.php
@@ -8,8 +8,6 @@ print_r($_REQUEST);
 print_r($_REQUEST["file"]);
 
 
-print_r($_FILES);
-
 if (!isset($_REQUEST["userid"]) || !isset($_REQUEST["projectid"]) || !isset($_REQUEST["tagid"]) || !isset($_REQUEST["file"]))
 {
 	die("Incorrect params");
