c595da4858bea67b2cdafa4fd1275f9206718f15
Manuel Cuevas <manuel@bexi.io>
2019-11-14 21:41:21 -0700

    Fix Bug

--- generator/ajax/uploadfile.php
+++ generator/ajax/uploadfile.php
@@ -1,7 +1,7 @@
 <?
-ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 
 include "../config.php";
 require '../vendor/autoload.php';
