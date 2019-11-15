cdd8d00e08882f75c75937aeef29906ac1845158
Adryx46 <adrian@bexi.io>
2019-11-15 12:42:13 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -1135,6 +1135,7 @@ function styles_ptags(){
   c.find("p").each(function(){
     if($(this).attr("class")==undefined||$(this).attr("class").search("bexi_editor")!=-1)
     {
+      console.log($(this).attr("class"));
       $(this).attr("style",$(this).attr("style")+"padding:0px;");
       $(this).css("padding","0px");
     }
