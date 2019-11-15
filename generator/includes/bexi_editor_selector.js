3b85b18fe8fbc722c1cfd1b5bd9ebcd5ee05e7f4
Manuel Cuevas <manuel@bexi.io>
2019-11-15 16:02:21 -0700

    Fix Bug

--- generator/includes/bexi_editor_selector.js
+++ generator/includes/bexi_editor_selector.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   		console.log("Project Selected");
   		//console.log('SelectProject|' + $(this).attr("bexi-code"));
 
-  		window.top.postMessage('SelectProject|' + $(this).attr("bexi-code"), '*')
+  		window.parent.postMessage('SelectProject|' + $(this).attr("bexi-code"), '*')
 
 /*
   		var myEvent = new CustomEvent('bexi_modu', { detail: {cmd: 'SelectProject', code: $(this).attr("bexi-code")} });
