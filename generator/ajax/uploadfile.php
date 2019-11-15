196d8307a18939875ad4fe3992dda311437db5e4
Manuel Cuevas <manuel@bexi.io>
2019-11-14 20:46:43 -0700

    Fix bug

--- generator/ajax/uploadfile.php
+++ generator/ajax/uploadfile.php
@@ -1,4 +1,38 @@
 <?
+include "../config.php";
+require '../vendor/autoload.php';
+
+include "../includes/utils.php";
+include "../includes/content_blocks.php";
+
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+
+// Delete the image.
+//try {
+
+$type = 1 ; // 1 = Landing page
+
+//echo "Tiempo 3 : ".(microtime(true) - $timeini)."<br>";
+
+$credentials = new Aws\Credentials\Credentials($aws_key, $aws_pass);
+
+$sdk = new Aws\Sdk([
+    'region'   => $AWS_REGION,
+    'version'  => 'latest',
+    'credentials' => $credentials
+]);
+
+$dynamodb = $sdk->createDynamoDb();
+$marshaler = new Marshaler();
+
+Crew\Unsplash\HttpClient::init([
+    'applicationId' => $unspash_appid,
+    'secret'        => $unspash_secret,
+    'callbackUrl'   => 'https://your-application.com/oauth/callback',
+    'utmSource' => 'Bexi Generator'
+]);
+
 
 $path = "/var/www/uploads.getmodu.com/public_html/";
 $webpath = "http://uploads.getmodu.com/";
@@ -8,28 +42,65 @@ $webpath = "http://uploads.getmodu.com/";
 //print_r($_FILES);
 
 
-if (!isset($_REQUEST["userid"]) || !isset($_REQUEST["projectid"]) || !isset($_FILES))
+if (((!isset($_REQUEST["userid"]) || !isset($_REQUEST["projectid"])) && (!isset($_REQUEST["devid"]))) || !isset($_FILES))
 {
 	die("Incorrect params");
 }
 
 $idfile =  (!isset($_REQUEST["tagid"])) ? uniqid() : $_REQUEST["tagid"];
 
+if (isset($_REQUEST["devid"]))
+{
+	$params = [
+        'TableName' => "modu_deliverables",
+         "KeyConditionExpression"=> "deliverable_id = :id",
+        "ExpressionAttributeValues"=> [
+            ":id" =>  ["S" => $_REQUEST["devid"]]
+        ]
+    ];
+
+    $result = $dynamodb->query($params);
+
+    if (count($result['Items'])>0)
+    {
+    	$projectid =  $marshaler->unmarshalValue($result['Items'][0]["project_id"]);	
+    }
+
+
+    $params = [
+        'TableName' => "modu_projects",
+         "KeyConditionExpression"=> "project_id = :id",
+        "ExpressionAttributeValues"=> [
+            ":id" =>  ["S" => $projectid]
+        ]
+    ];
+
+    $result = $dynamodb->query($params);
+
+    if (count($result['Items'])>0)
+    {
+    	$user =  $marshaler->unmarshalValue($result['Items'][0]["user_id"]);	
+    }
+    
+}else{
+	$userid = $_REQUEST["userid"];
+	$projectid = $_REQUEST["projectid"];
+}
 
 if (!$idfile || is_null($idfile) || $idfile== "null")
 {
 	$idfile = uniqid();
 }
 
-if (!file_exists($path.$_REQUEST["userid"])) {
-    mkdir($path.$_REQUEST["userid"], 0777, true);
+if (!file_exists($path.$userid)) {
+    mkdir($path.$userid, 0777, true);
 }
 
-if (!file_exists($path.$_REQUEST["userid"]."/".$_REQUEST["projectid"])) {
-    mkdir($path.$_REQUEST["userid"]."/".$_REQUEST["projectid"], 0777, true);
+if (!file_exists($path.$userid."/".$projectid)) {
+    mkdir($path.$userid."/".$projectid, 0777, true);
 }
 
-$fullpath= $path.$_REQUEST["userid"]."/".$_REQUEST["projectid"] . "/";
+$fullpath= $path.$userid."/".$projectid . "/";
 
 
 $target_file = $fullpath . basename($_FILES["file"]["name"]);
@@ -37,7 +108,7 @@ $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 $target_file = $fullpath."/".$idfile.".".$imageFileType;
-$webpath = $webpath.$_REQUEST["userid"]."/".$_REQUEST["projectid"] . "/".$idfile.".".$imageFileType;
+$webpath = $webpath.$userid."/".$projectid . "/".$idfile.".".$imageFileType;
 //print_r($_FILES);
 
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
