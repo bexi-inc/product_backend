e6d49894f9164e08b78e4b2c3b76538fd3843101
Manuel Cuevas <manuel@bexi.io>
2019-11-16 12:16:10 -0700

    Fix Bug

--- generator/load_theme.php
+++ generator/load_theme.php
@@ -36,38 +36,7 @@ $params = [
 
 $result = $dynamodb->query($params);
 
-//print_r($result);
-
-/*
-$contenido=$marshaler->unmarshalValue($result['Items'][0]['html']);
-$css[]=$marshaler->unmarshalValue($result['Items'][0]["css"]);
-
-*/
-
-$css_data = file_get_contents ($_REQUEST["file"]);
-
-$css_data = str_replace("%color1%", $marshaler->unmarshalValue($result['Items'][0]["color1"]), $css_data);
-$css_data = str_replace("%color2%", $marshaler->unmarshalValue($result['Items'][0]["color2"]), $css_data);
-$css_data = str_replace("%color3%", $marshaler->unmarshalValue($result['Items'][0]["color3"]), $css_data);
-$css_data = str_replace("%color4%", $marshaler->unmarshalValue($result['Items'][0]["color4"]), $css_data);
-$css_data = str_replace("%color5%", $marshaler->unmarshalValue($result['Items'][0]["color5"]), $css_data);
-
-if (!$_REQUEST["fontid"])
-{
-	$_REQUEST["fontid"]=1;
-}
-$params = [
-    'TableName' => "bexi_prod_fonts",
-     "KeyConditionExpression"=> "id = :vId",
-    "ExpressionAttributeValues"=> [
-        ":vId" =>  ["S" => $_REQUEST["fontid"]]
-    ]
-];
+print_r($result);
 
 
-$result = $dynamodb->query($params);
-
-$css_data = str_replace("%font1%", '"'.$marshaler->unmarshalValue($result['Items'][0]["name"]).'"', $css_data);
-
-echo $css_data;
 ?>
