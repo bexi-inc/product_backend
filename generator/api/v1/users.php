9340eca78542faee8c0508b02b5de220f51f8667
Manuel Cuevas <manuel@bexi.io>
2019-11-15 20:52:36 -0700

    Fix Error

--- generator/api/v1/users.php
+++ generator/api/v1/users.php
@@ -339,7 +339,6 @@ function GmailSigin($code, $redirect="")
 	{
 		$client->setRedirectUri($redirect);
 	}else{
-		print_r(GMAIL_CLIENT_REDIRECT_URL);
 		$client->setRedirectUri(GMAIL_CLIENT_REDIRECT_URL);
 	}
 	
