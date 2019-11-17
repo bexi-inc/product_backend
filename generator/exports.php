d585ee77abb41d65f4f4140b3206c3f7eda8849b
Manuel Cuevas <manuel@bexi.io>
2019-11-16 17:01:38 -0700

    Fix Error

--- generator/exports.php
+++ generator/exports.php
@@ -218,7 +218,9 @@ ob_start();
             $img = [];
             $img["old_src"] = $old_src;
             $filename = pathinfo($old_src,PATHINFO_EXTENSION );
-            $img["new_src"] = "./files/imgs/".$filename;
+            $img["filename"] = $filename;
+            $img["new_src"] = "./files/img/".$filename;
+            $tag->setAttribute('src',$img["new_src"]);
             $images[] = $img;
         }
         $pos = strpos(pathinfo($old_src,PATHINFO_DIRNAME),"uploads.getmodu.com");
@@ -289,6 +291,11 @@ if (!file_exists($PATH."/files/")) {
      mkdir($PATH."/files/", 0777, true);
 }
 
+$PATHIMG .= "/img/"
+if (!file_exists($PATHIMG)) {
+     mkdir($PATHIMG, 0777, true);
+}
+
 $PATHFILES= $PATH."/files/";
 
 ob_start();
@@ -307,6 +314,11 @@ fclose($myfile);
 copy("includes/jquery-ui.min.css", $PATHFILES."jquery-ui.min.css" );
 copy("includes/jquery-3.4.1.min.js", $PATHFILES."jquery-3.4.1.min.js" );
 copy("css/bexi.css", $PATHFILES."bexi.css" );
+
+foreach ($images as $img)
+{
+    copy($img["old_src"], $PATHFILES.$img["filename"] );
+}
 
 zipme($PATH,$PATHBASE.$project_name.".zip");
 
