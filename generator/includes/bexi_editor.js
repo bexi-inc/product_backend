f755e762c11eb7db23fc6b02691074b71403527f
Adryx46 <adrian@bexi.io>
2019-11-16 21:30:28 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -1482,7 +1482,7 @@ function initialize_editors_text(fonts){
           auto_save();
       },
       'image.beforeUpload': function (images) {
-        console.log(this.$doc[0].activeElement.files[0],"response before");
+        console.log(this.$doc[0].activeElement,"response before");
         console.log(images,"response before");
         //this.opts.imageUploadParams.tagid=window.bexi_tagid;
       },
