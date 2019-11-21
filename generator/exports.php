4b9aa4edb504b38e75fcfeb3338ef7122f4a259a
Manuel Cuevas <manuel@bexi.io>
2019-11-20 20:58:45 -0700

    Fix Bug

--- generator/exports.php
+++ generator/exports.php
@@ -1,8 +1,8 @@
 <?
-
+/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 include "includes/global.php";
 include "includes/utils.php";
     
