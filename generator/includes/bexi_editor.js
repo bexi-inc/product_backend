06752b644289e9276b33073512cd3baca08a86f5
Adryx46 <adrian@bexi.io>
2019-11-25 12:21:05 -0700

    Update bexi_editor.js

--- generator/includes/bexi_editor.js
+++ generator/includes/bexi_editor.js
@@ -1573,8 +1573,7 @@ function initialize_editors_text(){
       'image.beforeUpload': function (images) {
         console.log("before");
         var res=save_img(window.bexi_tagid,images[0]);
-        $.when($(res)).then(function(data, status, xhr) {
-          // Lógica de respuesta aquí.
+        res.done(function(data){
           window.response_img.push(data);
         });
         console.log(window.response_img);
