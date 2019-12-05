efe0e0e08c83a247fec2c0ea9257e202514b771f
Manuel Cuevas <manuel@bexi.io>
2019-12-05 12:34:02 -0700

    Fix Error

--- generator/includes/utils.php
+++ generator/includes/utils.php
@@ -213,7 +213,7 @@ function setImages($contenido,$keywords = ""){
 				{
 					$pos_class2 = strpos($taghtml,'"',$pos_class);
 				}
-				echo $tagstar,"=>".$pos_class2;
+				echo $tagstar."=>".$pos_class2;
 				//echo "add bexi_img";
 				$contenido = substr_replace($contenido, "bexi_img ", $tagstar + $pos_class2 + 1  ,0);
 				
