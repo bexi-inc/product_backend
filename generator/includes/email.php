57d41e30f64acd4af260d37003018ea023987a52
Manuel Cuevas <manuel@bexi.io>
2019-12-06 09:12:19 -0700

    Add themes email

---
+++ generator/includes/email.php
@@ -0,0 +1,12 @@
+<?
+
+/ Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// If necessary, modify the path in the require statement below to refer to the
+// location of your Composer autoload.php file.
+require '../vendor/autoload.php';
+
+?>
