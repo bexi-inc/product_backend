cd90aff8aa6241981fdd5db89d22941e2824a0e5
Manuel Cuevas <manuel@bexi.io>
2019-12-09 11:40:41 -0700

    Fix Debug

--- generator/api/v1/users.php
+++ generator/api/v1/users.php
@@ -352,8 +352,6 @@ function GmailSigin($code, $redirect="")
 	if ($token["error"])
 	{
 		$ret["error"]= $token["error"]." - ".$token["error_description"];
-		print_r($client);
-		print_r($token);
 	}else
 	{
 		$client->setAccessToken($token['access_token']);
