c8da8935f2aea88ae059ce52cb148e71a46018f9
Adryx46 <adrian@bexi.io>
2019-12-10 14:28:35 -0700

    Senders emails

--- generator/ajax/sendform_download.php
+++ generator/ajax/sendform_download.php
@@ -1,23 +1,27 @@
-<?
+<?php
 
  header("Access-Control-Allow-Origin: *");
 // this is just a test
 //send back to the ajax request the request
 
-	$data = json_encode($_POST);
+    $data = json_encode($_POST);
+    
+    $data = json_decode($data,true);
 
 	$to = $data["email_to"];
     $subject = "Modu - Form";
+    $data = json_encode($data);
     $message = print_r($data,true);
     $from = "noreply@getmodu.com";
    	$headers = "From: getModu.com";
 
-   	$res["error"] = "";
     if (mail($to, $subject, $message, $headers)) {
-        $res["msj"] = "Mail Sent.";
+        $res["error"] = 0;
+	    $res["error_msj"] = "Mail Sent.";
     }
     else {
-        $res["error"] = "failed";
+        $res["error"] = 500;
+	    $res["error_msj"] = "Unable to send";
     }
 
     echo json_encode($res);
