4b37c1ed5227f5a0ff3d92d10c99cc0fa1d771cc
Manuel Cuevas <manuel@bexi.io>
2019-12-12 11:13:22 -0700

    Debug

--- generator/test_email.php
+++ generator/test_email.php
@@ -5,7 +5,7 @@ $message = "Line 1\r\nLine 2\r\nLine 3";
 
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
 $message = wordwrap($message, 70, "\r\n");
-
+$headers = "From: MANUEL CUEVAS <hello@getmodu.com>\r\n";
 // Send
-print_r(mail('admin@vikingosol.com', 'My Subject', $message));
+print_r(mail('admin@vikingosol.com', 'My Subject', $message, $headers));
 ?>
