def9982f1346c71a4c608750788ce5a0eda6b0ec
LeonardoCervantes95 <leo@bexi.io>
2019-12-13 12:15:36 -0700

    emails.php and sendform.php Update

--- generator/api/v1/emails.php
+++ generator/api/v1/emails.php
@@ -197,6 +197,101 @@ function SendEmail($type,$user,$IdRef = 0, $data)
 	}
 }
 
+function SendEmailForm($type,$user,$IdRef = 0, $data) 
+{
+	global $aws_key, $aws_pass, $AWS_REGION;
+	$credentials = new Aws\Credentials\Credentials($aws_key, $aws_pass);
+
+	$sdk = new Aws\Sdk([
+    'region'   => $AWS_REGION,
+    'version'  => 'latest',
+    'credentials' => $credentials
+	]);
+
+	$dynamodb = $sdk->createDynamoDb();
+	$marshaler = new Marshaler();
+
+	$code=//data;
+	$subject = //"lo que envíe";
+
+	// O
+	$params = [
+        'TableName' => "bexi_prod_users",
+         "KeyConditionExpression"=> "id = :id",
+        "ExpressionAttributeValues"=> [
+            ":id" =>  ["S" => strval($user)],
+        ]
+    ];
+
+    //print_r($params);
+     $result = $dynamodb->query($params);
+
+     //print_r($result['Items']);
+     $user_email = $marshaler->unmarshalValue($result['Items'][0]["username"]);
+
+	// Replace sender@example.com with your "From" address.
+	// This address must be verified with Amazon SES.
+	$sender = EMAIL_FROM;										//AQUI
+	$senderName = EMAIL_FROM_NAME;
+
+	// Replace recipient@example.com with a "To" address. If your account
+	// is still in the sandbox, this address must be verified.
+	$recipient = $user_email;
+
+	// Replace smtp_username with your Amazon SES SMTP user name.
+	$usernameSmtp = SMTP_USER;
+
+	// Replace smtp_password with your Amazon SES SMTP password.
+	$passwordSmtp = SMTP_PASS;
+
+	// Specify a configuration set. If you do not want to use a configuration
+	// set, comment or remove the next line.
+	//$configurationSet = 'ConfigSet';
+
+	// If you're using Amazon SES in a region other than US West (Oregon),
+	// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
+	// endpoint in the appropriate region.
+	$host = 'email-smtp.'.AWS_REGION2.'.amazonaws.com';
+	$port = 587;
+
+	// The subject line of the email
+	//$subject = 'Amazon SES test (SMTP interface accessed using PHP)';
+
+	
+
+	$mail = new PHPMailer(true);
+
+	try {
+	    // Specify the SMTP settings.
+	    $mail->isSMTP();
+	    $mail->setFrom($sender, $senderName); //de BD
+	    $mail->Username   = $usernameSmtp;
+	    $mail->Password   = $passwordSmtp;
+	    $mail->Host       = $host;
+	    $mail->Port       = $port;
+	    $mail->SMTPAuth   = true;
+	    $mail->SMTPSecure = 'tls';
+	    $mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
+
+	    // Specify the message recipients.
+	    $mail->addAddress($recipient);
+	    // You can also add CC, BCC, and additional To recipients here.
+
+	    // Specify the content of the message.
+	    $mail->isHTML(true);
+	    $mail->Subject    = $subject;   //cambiar
+	    $mail->Body       = $code;		//cambiar
+	    //$mail->AltBody    = $bodyText;
+
+	  	//print_r($mail);
+	    $mail->Send();
+	   //echo "Email sent!" , PHP_EOL;
+	} catch (phpmailerException $e) {
+	    echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
+	} catch (Exception $e) {
+	    echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+	}
+}
 
 	/*
 	//include("../../config.php");
