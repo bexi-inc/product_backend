66fb5064f50c044abd3fa0a98b102aba60f4ff4e
Manuel Cuevas <manuel@bexi.io>
2019-12-13 20:10:02 -0700

    Fix Bug

--- generator/api/v1/emails.php
+++ generator/api/v1/emails.php
@@ -10,8 +10,8 @@ use Aws\DynamoDb\Marshaler;
 
 // If necessary, modify the path in the require statement below to refer to the
 // location of your Composer autoload.php file.
-require '../../vendor/autoload.php';
-require_once ("../../config.php");
+include_once ('../../vendor/autoload.php');
+include_once ("../../config.php");
 
 
 date_default_timezone_set('UTC');
