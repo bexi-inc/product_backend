c596c72edbd04bebaa16d0002c76341a33ccfd42
Adryx46 <adrian@bexi.io>
2019-12-18 10:25:14 -0700

    Update generator.php

--- generator/generator.php
+++ generator/generator.php
@@ -402,9 +402,9 @@ OBTENEMOS EL LOGO EN BASE AL PROJECT ID
     echo '<link href="https://cdn.jsdelivr.net/npm/froala-editor@3.0.6/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@3.0.6/js/froala_editor.pkgd.min.js"></script>';
     */
-    echo "<link href='https://cdn.jsdelivr.net/npm/froala-editor@2.9.6/css/froala_editor.min.css' rel='stylesheet' type='text/css' />";
-    echo "<link href='https://cdn.jsdelivr.net/npm/froala-editor@2.9.6/css/froala_style.min.css' rel='stylesheet' type='text/css' />";
-    echo "<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/froala-editor@2.9.6/js/froala_editor.min.js'></script>";
+    echo "https://editor-latest.s3.amazonaws.com/js/froala_editor.pkgd.min.js";
+    echo "https://editor-latest.s3.amazonaws.com/css/froala_editor.pkgd.min.css";
+    echo "https://editor-latest.s3.amazonaws.com/css/froala_style.min.css";
 
     echo'<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@3.0.6/js/third_party/font_awesome.min.js"></script>';
 
