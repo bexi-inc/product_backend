0244a8c2972a0125e39dcc5cae488f6afd00c22d
Adryx46 <adrian@bexi.io>
2019-12-18 13:55:36 -0700

    Update projects.php

--- generator/api/v1/projects.php
+++ generator/api/v1/projects.php
@@ -164,13 +164,14 @@ function GetProjects($userId)
 				$userData2 ='{
 					":projectid" : "'.$proj["project_id"].'"
 				}';
-				$table2 = ExecuteQuery("modu_deliverables",$userData2,"project_id = :projectid", "project_id-index" , "" , false);
+				$table2 = ExecuteQuery("modu_deliverables",$userData2,"project_id = :projectid", "lastproject_id-index" , "" , false);
 				if ($table2["error"]=="")
 				{
 					$dbdata2 = $table2["data"]['Items'];
 					if (count($dbdata2)>0)
 					{
-						$proj["deliverable_id"]=$Marshaler->unmarshalValue($dbdata2[0]["deliverable_id"]);
+						$last=count($dbdata2)-1;
+						$proj["deliverable_id"]=$Marshaler->unmarshalValue($dbdata2[$last]["deliverable_id"]);
 					}
 				}
 				else
