2cad438615c8204c4a811fcfb1a8cfa0d5b2a2a4
LeonardoCervantes95 <leo@bexi.io>
2019-12-19 16:18:09 -0700

    Update module_viewer.php

--- generator/module_viewer.php
+++ generator/module_viewer.php
@@ -1,224 +1,41 @@
-<?
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Module Viewer</title>
+    <script src="includes/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/bexi.css" >
+</head>
+<body>
 
-include "includes/global.php";
-include "config.php";
-include "includes/utils.php";
-require 'vendor/autoload.php';
-
-
-date_default_timezone_set('UTC');
-
-use Aws\DynamoDb\Exception\DynamoDbException;
-use Aws\DynamoDb\Marshaler;
-
-
-$credentials = new Aws\Credentials\Credentials($aws_key, $aws_pass);
-
-$sdk = new Aws\Sdk([
-    'region'   => 'us-east-2',
-    'version'  => 'latest',
-    'credentials' => $credentials
-]);
-
-
-Crew\Unsplash\HttpClient::init([
-    'applicationId' => $unspash_appid,
-    'secret'        => $unspash_secret,
-    'callbackUrl'   => 'https://your-application.com/oauth/callback',
-    'utmSource' => 'Bexi Generator'
-]);
-
-
-
-
-$dynamodb = $sdk->createDynamoDb();
-$marshaler = new Marshaler();
-
-
-
-$eav = $marshaler->marshalJson('
-    {
-        ":vtipo": "hea" 
-    }
-');
-
-$params = [
-    'TableName' => "bexi_prod_contentblock",
-     "KeyConditionExpression"=> "id = :vid",
-    "ExpressionAttributeValues"=> [
-        ":vid" =>  ["S" => $_REQUEST["id"]]
-    ]
-];
-
-//$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-
-$contenido="";
-
- $result = $dynamodb->query($params);
-
-$contenido=$marshaler->unmarshalValue($result['Items'][0]['html_code']);
-$css[]=$marshaler->unmarshalValue($result['Items'][0]["file_css"]);
-
-$contenido = setImages($contenido,"");
-
-
-$pos = 0;
-$pos2 = 0;
-while ( ( $pos = strpos( $contenido, "%id%", $pos ) ) !== false ) {
-  $pos2 = strpos( $contenido, "%", ($pos + 1) );
-  $idrand = uniqid('bexi_');
-  $contenido=substr_replace($contenido,$idrand,$pos,4);
-}
-
-
-
-echo "<!doctype html>";
-echo "\r\n";
-echo "<html>";
-echo "\r\n";
-echo "<head>";
-echo "\r\n";
-echo "<meta charset='utf-8'>";
-echo "\r\n";
-echo "<title>Bexi DNA Project</title>";
-echo "\r\n";
-
-echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-echo '<script src="includes/jquery-3.4.1.min.js"></script>';
-echo '<link rel="stylesheet" href="includes/jquery-ui.min.css">';
-echo '<link rel="stylesheet" href="./css/bexi_panel.css" >';
-echo '<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>';
-echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
-echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>';
-
-/**************   FROALA EDITOR **************/
-
-echo '<link href="https://cdn.jsdelivr.net/npm/froala-editor@3.0.6/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@3.0.6/js/froala_editor.pkgd.min.js"></script>';
-echo'<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@3.0.6/js/third_party/font_awesome.min.js"></script>';
-
-echo'<link rel="stylesheet" type="text/css" href="css/bexi_generator.css" >';
-
-echo'<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" >';
-
-/**************   ICON FONTS **************/
-echo'<script src="https://kit.fontawesome.com/2fd6605c8f.js" crossorigin="anonymous"></script>';
-
-//echo'<link rel="stylesheet" type="text/css" href="load_css.php?file=./css/bexi.php&id='.$id_color.'&fontid='.$FontId.'" id="mod_css_0" >'; 
-
-/**************   jquery dialog  **************/
-echo '<script src="includes/jquery-ui.min.js"></script>';
-echo '<link rel="stylesheet" type="text/css" href="includes/jquery-ui.theme.css" >';
-
-/**************   jquery colorpicker  **************/
-echo '<link rel="stylesheet" type="text/css" media="all" href="includes/css/jquery.minicolors.css">
-      <script type="text/javascript" src="includes/jquery.minicolors.min.js"></script>';
-
-/**************   PAGINATION **************/
-echo '<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.js"></script>';
-echo '<script src="includes/pagination.js"></script>';
-echo'<link rel="stylesheet" type="text/css" href="includes/css/bs-pagination.css" >';
-
-/************** GOOGLE FONTS  FROALA EDITOR **************/
-echo'<script src="includes/visibility.js"></script>';
-echo'<script src="./config.js"></script>';
-
-echo'<link rel="stylesheet" type="text/css" href="css/bexi.css" >';
-
-echo'<link rel="stylesheet" type="text/css" href="includes/css/bexi_editor.css" >';
-echo '<script type="text/javascript"  src="includes/bexi_editor.js"></script>';
-
-//echo $FontImport;
-echo "\r\n";
-?>
+<div class="bexi_container">
+    <div class="row justify-content-center py-5" style="position: relative;">
+        <div class="col-md-6 C p-5 shadow_box" style="background-color: white;">
+            <h2 id="%id%" class="bexi_title Black C">Digital App</h2>
+            <p id="%id%" class="bexi_text Lead_text Gray C p-0">
+                <form name="form" action="" method="get">
+                    <input type="text" name="subject" id="subject" value="Car Loan">
+                </form>
+            </p>
+            <p id="%id%" class="mt-4 C">
+                <a class="btn btn-primary bexi_button" href="#" style="width: auto;">Download</a>
+            </p>
+        </div>
+    </div>
+</div>
     
-<?
-echo "</head>";
-echo "\r\n";
+    <script>
 
-echo "<body>";
-echo "<div id='modu_main'>";
-echo $contenido;
-echo  "</div>";
+    $("#subject").on('keyup', function (e) {
+        if (e.keyCode === 13) {
+            alert('ENTER');
+        }
+    });
 
-echo ' <script>
-            anima_isHidden = function(e) {
-                if (!(e instanceof HTMLElement)) return !1;
-                if (getComputedStyle(e).display == "none") return !0; else if (e.parentNode && anima_isHidden(e.parentNode)) return !0;
-                return !1;
-            };
-            anima_loadAsyncSrcForTag = function(tag) {
-                var elements = document.getElementsByTagName(tag);
-                var toLoad = [];
-                for (var i = 0; i < elements.length; i++) {
-                    var e = elements[i];
-                    var src = e.getAttribute("src");
-                    var loaded = (src != undefined && src.length > 0 && src != "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
-                    if (loaded) continue;
-                    var asyncSrc = e.getAttribute("anima-src");
-                    if (asyncSrc == undefined || asyncSrc.length == 0) continue;
-                    if (anima_isHidden(e)) continue;
-                    toLoad.push(e);
-                }
-                toLoad.sort(function(a, b) {
-                    return anima_getTop(a) - anima_getTop(b);
-                });
-                for (var i = 0; i < toLoad.length; i++) {
-                    var e = toLoad[i];
-                    var asyncSrc = e.getAttribute("anima-src");
-                    e.setAttribute("src", asyncSrc);
-                }
-            };
-            anima_pauseHiddenVideos = function(tag) {
-                var elements = document.getElementsByTagName("video");
-                for (var i = 0; i < elements.length; i++) {
-                    var e = elements[i];
-                    var isPlaying = !!(e.currentTime > 0 && !e.paused && !e.ended && e.readyState > 2);
-                    var isHidden = anima_isHidden(e);
-                    if (!isPlaying && !isHidden && e.getAttribute("autoplay") == "autoplay") {
-                        e.play();
-                    } else if (isPlaying && isHidden) {
-                        e.pause();
-                    }
-                }
-            };
-            anima_loadAsyncSrc = function(tag) {
-                anima_loadAsyncSrcForTag("img");
-                anima_loadAsyncSrcForTag("iframe");
-                anima_loadAsyncSrcForTag("video");
-                anima_pauseHiddenVideos();
-            };
-            var anima_getTop = function(e) {
-                var top = 0;
-                do {
-                    top += e.offsetTop || 0;
-                    e = e.offsetParent;
-                } while (e);
-                return top;
-            };
-            anima_loadAsyncSrc();
-            anima_old_onResize = window.onresize;
-            anima_new_onResize = undefined;
-            anima_updateOnResize = function() {
-                if (anima_new_onResize == undefined || window.onresize != anima_new_onResize) {
-                    anima_new_onResize = function(x) {
-                        if (anima_old_onResize != undefined) anima_old_onResize(x);
-                        anima_loadAsyncSrc();
-                    };
-                    window.onresize = anima_new_onResize;
-                    setTimeout(function() {
-                        anima_updateOnResize();
-                    }, 3000);
-                }
-            };
-            anima_updateOnResize();
-            setTimeout(function() {
-                anima_loadAsyncSrc();
-            }, 200);
-        </script>';
-echo "\r\n";
-echo "</body>";
-echo "\r\n";
-echo "</html>";
-echo "\r\n";
-?>
+    $('#abc_frame').attr('src', url);
+    </script>
+
+</body>
+</html>
