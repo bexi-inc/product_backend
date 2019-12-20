3f115e13132419519af0d6ff4ebe965c4e8b10e2
LeonardoCervantes95 <leo@bexi.io>
2019-12-20 16:00:11 -0700

    bug fix

--- generator/module_viewer.php
+++ generator/module_viewer.php
@@ -13,7 +13,7 @@
     <style>
         
         .iframe-container {
-            min-height: calc(103vh - 100px);
+            min-height: calc(102vh - 100px);
         }
         
         .iframe-container iframe {
@@ -21,7 +21,7 @@
         left: 0;
         top: 0;
         width: 100%;
-        min-height: calc(103vh - 100px);
+        min-height: calc(102vh - 100px);
         }
         
         /* 4x3 Aspect Ratio */
