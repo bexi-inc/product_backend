cac297a0477f3dfc8275943076f3de5717c9e9e0
LeonardoCervantes95 <leo@bexi.io>
2019-12-19 23:13:14 -0700

    Update module_viewer.php

--- generator/module_viewer.php
+++ generator/module_viewer.php
@@ -31,6 +31,26 @@
             height: 100%;
         }  
         }
+        .iframe-container {
+        overflow: hidden;
+        padding-top: 56.25%;
+        position: relative;
+        }
+        
+        .iframe-container iframe {
+        border: 0;
+        height: 100%;
+        left: 0;
+        position: absolute;
+        top: 0;
+        width: 100%;
+        }
+        
+        /* 4x3 Aspect Ratio */
+        .iframe-container-4x3 {
+        padding-top: 75%;
+        }
+        
         #viewer{
             zoom: 0.71;
             -moz-transform:scale(0.75);
