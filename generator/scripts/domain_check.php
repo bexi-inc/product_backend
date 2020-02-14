fbddd59b111737ec6bbc86667514e982041df0be
Manuel Cuevas <manuel@bexi.io>
2020-02-13 17:00:36 -0700

    fix Bug

--- generator/scripts/domain_check.php
+++ generator/scripts/domain_check.php
@@ -58,7 +58,8 @@ foreach ($result['Items'] as $subd)
 
 		$key = $marshaler->marshalJson('
 		    {
-		        "deliverable_id " : "' . $deliverable . '"
+		        "deliverable_id " : "' . $deliverable . '",
+		        "project_id " : "'.$result2['Items'][0]["project_id "].'"
 		    }
 		');
 
