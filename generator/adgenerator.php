8993d729ed3eb63bdf9dd232a1db9e3de1b99f98
chrisgarcia96 <60152967+chrisgarcia96@users.noreply.github.com>
2020-03-04 12:45:54 -0700

    Update adgenerator.php

--- generator/adgenerator.php
+++ generator/adgenerator.php
@@ -267,7 +267,7 @@ for ($i=1; $i < $count ; $i++) {
 echo '];';
 echo 'var t2maxchar="'.(($xblock*2)/(0.2*($yblock/2))).'";';
 echo 'var t2size=[8';
-$total=($yblock/2)-8;
+$total=($yblock/4)-8;
 $count=$total/2;
 for ($i=1; $i < $count ; $i++) {
   echo ','.(($i*2)+8);
