e170ea6a27e81e751b6774de073ddbb45c1ccfe2
Adryx46 <adrian@bexi.io>
2020-03-09 12:29:23 -0700

    ADS Selector and generator

---
+++ generator/ad_selector.php
@@ -0,0 +1,268 @@
+<?
+
+include "config.php";
+require 'vendor/autoload.php';
+
+include "includes/utils.php";
+include "includes/content_blocks.php";
+
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+
+
+$credentials = new Aws\Credentials\Credentials($aws_key, $aws_pass);
+
+$sdk = new Aws\Sdk([
+    'region'   => $AWS_REGION,
+    'version'  => 'latest',
+    'credentials' => $credentials
+]);
+
+$dynamodb = $sdk->createDynamoDb();
+$marshaler = new Marshaler();
+
+Crew\Unsplash\HttpClient::init([
+    'applicationId' => $unspash_appid,
+    'secret'        => $unspash_secret,
+    'callbackUrl'   => 'https://your-application.com/oauth/callback',
+    'utmSource' => 'Bexi Generator'
+]);
+
+$keywords= "";
+if (isset($_REQUEST["campaignid"]))
+{
+	$campaign_id = $_REQUEST["campaignid"];
+	$params = [
+        'TableName' => "modu_campaigns",
+         "KeyConditionExpression"=> "id = :id",
+        "ExpressionAttributeValues"=> [
+            ":id" =>  ["S" => $campaign_id]
+        ]
+    ];
+
+    $result_proj = $dynamodb->query($params);
+
+    if (count($result_proj["Items"])>0)
+    {
+
+        if (isset($result_proj['Items'][0]["keywords"]) && !is_null($result_proj['Items'][0]["keywords"]))
+        {
+            $keywords=$marshaler->unmarshalValue($result_proj['Items'][0]["keywords"]);
+        }else
+        {
+            $keywords="";
+		}
+
+		if (isset($result_proj['Items'][0]["recipe_type"]) && !is_null($result_proj['Items'][0]["recipe_type"]))
+        {
+            $recipe_type=$marshaler->unmarshalValue($result_proj['Items'][0]["recipe_type"]);
+        }else
+        {
+            $recipe_type="1";
+		}
+
+    }else{
+		$keywords = "";
+		$recipe_type="1";
+    }
+}
+
+
+?><!DOCTYPE html>
+<html>
+<head>
+	<script src="includes/jquery-3.4.1.min.js"></script>
+	<style type="text/css">
+			/* Variables */
+	:root {
+	/*--thumbnail-width : 1440px;*/
+	--thumbnail-height : 280vh;
+	--thumbnail-zoom: 0.35;
+	}
+
+	/* Basic CSS Reset */
+	* {
+	  margin: 0;
+	  padding: 0;
+	  box-sizing: border-box;
+	}
+
+	/* Demo-specific styling */
+	body {
+	  text-align: center;
+	  /*margin-top: 50px;*/
+	}
+
+	/* This container helps the thumbnail behave as if it were an unscaled IMG element */
+		  /*width: calc(var(--thumbnail-width) * var(--thumbnail-zoom));*/
+	 /*width: 36%;*/
+	  /*height: calc(var(--thumbnail-height) * var(--thumbnail-zoom));
+	  	  overflow: hidden;
+	  */
+	  	  /*border-style: solid;*/	
+	.thumbnail-container {
+	  height: 100%;
+	  display: inline-block;
+	  position: absolute;
+	  top: 0;
+	  background-color: transparent;
+	  float: left;
+	  height: calc(var(--thumbnail-height)+50px);
+		width: calc(var(--thumbnail-width)+50px);
+	}
+
+	
+
+	/* This is a masking container for the zoomed iframe element */
+	.thumbnail {
+	  -ms-zoom: var(--thumbnail-zoom);
+	  -moz-transform: scale(var(--thumbnail-zoom));
+	  -moz-transform-origin: 0 0;
+	  -o-transform: scale(var(--thumbnail-zoom));
+	  -o-transform-origin: 0 0;
+	  -webkit-transform: scale(var(--thumbnail-zoom));
+	  -webkit-transform-origin: 0 0;
+	}
+
+	/* This is our screen sizing */
+	.thumbnail, .thumbnail iframe {
+	  width: var(--thumbnail-width);
+	  height: var(--thumbnail-height);
+	}
+
+	/* This facilitates the fade-in transition instead of flicker. It also helps us maintain the illusion that this is an image, since some webpages will have a preloading animation or wait for some images to download */
+	.thumbnail iframe {
+	  margin:25px;
+	  /*opacity: 0.7;*/
+	  transition: all 300ms ease-in-out;
+	  border-radius:25px;
+	  -webkit-box-shadow:0 5px 20px rgba(81,91,104,.2),0 -5px 10px rgba(81,91,104,.2);
+	  box-shadow:0 5px 20px rgba(81,91,104,.2),0 -5px 10px rgba(81,91,104,.2);
+	}
+
+	/* This pseudo element masks the iframe, so that mouse wheel scrolling and clicking do not affect the simulated "screenshot" */
+	/*.thumbnail:after {
+	  content: "";
+	  display: block;
+	  position: absolute;
+	  top: 0;
+	  left: 0;
+	  right: 10px;
+	  bottom: 0;
+	}*/
+
+	.main_selector
+	{
+		width: 100%;
+		height: 100%;
+		position: absolute;
+	  	margin: auto;
+
+	}
+
+	/* Next & previous buttons */
+	.selector_prev, .selector_next {
+	  cursor: pointer;
+	  position: absolute;
+	  top: 50%;
+	  width: auto;
+	  margin-top: -22px;
+	  padding: 16px;
+	  color: white;
+	  font-weight: bold;
+	  font-size: 18px;
+	  transition: 0.6s ease;
+	  border-radius: 0 3px 3px 0;
+	  user-select: none;
+	  z-index: 999;
+	}/*	  background-color: rgba(0,0,0,0.3); */
+
+	/* Position the "next button" to the right */
+	.selector_next {
+	  margin-right:15px;
+	  right: 0;
+	  border-radius: 3px 0 0 3px;
+	  background-image: url("./imgs/right_gray.png");
+	  background-position: center; /* Center the image */
+	  background-repeat: no-repeat; /* Do not repeat the image */
+	  background-size: cover; /* Resize the background image to cover the entire container */
+	}
+
+	.selector_prev {
+	  margin-left:15px;
+	  left: 0;
+	  border-radius: 3px 0 0 3px;
+	  background-image: url("./imgs/left_gray.png");
+	  background-position: center; /* Center the image */
+	  background-repeat: no-repeat; /* Do not repeat the image */
+	  background-size: cover; /* Resize the background image to cover the entire container */
+	}
+
+	/* On hover, add a black background color with a little bit see-through */
+	.selector_prev:hover{
+		background-image: url("./imgs/left.png");
+	}/*background-color: rgba(0,0,0,0.7); */
+
+	.selector_next:hover {
+		background-image: url("./imgs/right.png");
+	}/*background-color: rgba(0,0,0,0.7); */
+/*
+	.mySlides:hover:after{
+		  content: url('imgs/edit.png');;
+		  width: 100%;
+		  height: 100%;
+		  position: absolute;
+		  left: 0;
+		  top: 0;
+		  padding-top: 50%;
+		  background-color:rgba(0, 0, 0, 0.3);
+	}*/
+	.thumbnail{
+		height: calc(var(--thumbnail-height)+50px);
+		width: calc(var(--thumbnail-width)+50px);
+		background-color: transparent;
+		position:relative;
+	}/*	background-color: #000; */ 
+
+
+	.FrameSelected {
+	  -webkit-box-shadow:0 5px 20px rgba(45, 211, 214,.2),0 -5px 10px rgba(45, 211, 214,.2) !important;
+	  box-shadow:0 15px 30px rgba(45, 211, 214,.9),0 5px 20px rgba(45, 211, 214,.9) !important;
+	}
+
+	.project_inactive
+	{
+		opacity : 0.2;
+	}
+
+	.visible{
+		visibility: visible;
+	}
+
+	.hidden{
+		visibility: hidden;
+	}
+
+	.Spinner{position:relative;display:block;width:1em;height:1em;margin:0 auto;font-size:192px}.Spinner-inner{position:absolute;width:.078125em;height:.078125em;border-radius:50%;background-color:#2dd3d6;-webkit-animation:Spinner-animation 1.2s linear infinite;-moz-animation:Spinner-animation 1.2s linear infinite;-o-animation:Spinner-animation 1.2s linear infinite;-ms-animation:Spinner-animation 1.2s linear infinite;animation:Spinner-animation 1.2s linear infinite}.Spinner-inner:first-child{top:.453125em;left:.828125em;-webkit-animation-delay:0;-moz-animation-delay:0;-o-animation-delay:0;-ms-animation-delay:0;animation-delay:0}.Spinner-inner:nth-child(2){top:.28125em;left:.78125em;-webkit-animation-delay:-.1s;-moz-animation-delay:-.1s;-o-animation-delay:-.1s;-ms-animation-delay:-.1s;animation-delay:-.1s}.Spinner-inner:nth-child(3){top:.140625em;left:.640625em;-webkit-animation-delay:-.2s;-moz-animation-delay:-.2s;-o-animation-delay:-.2s;-ms-animation-delay:-.2s;animation-delay:-.2s}.Spinner-inner:nth-child(4){top:.09375em;left:.453125em;-webkit-animation-delay:-.3s;-moz-animation-delay:-.3s;-o-animation-delay:-.3s;-ms-animation-delay:-.3s;animation-delay:-.3s}.Spinner-inner:nth-child(5){top:.140625em;left:.28125em;-webkit-animation-delay:-.4s;-moz-animation-delay:-.4s;-o-animation-delay:-.4s;-ms-animation-delay:-.4s;animation-delay:-.4s}.Spinner-inner:nth-child(6){top:.28125em;left:.140625em;-webkit-animation-delay:-.5s;-moz-animation-delay:-.5s;-o-animation-delay:-.5s;-ms-animation-delay:-.5s;animation-delay:-.5s}.Spinner-inner:nth-child(7){top:.453125em;left:.09375em;-webkit-animation-delay:-.6s;-moz-animation-delay:-.6s;-o-animation-delay:-.6s;-ms-animation-delay:-.6s;animation-delay:-.6s}.Spinner-inner:nth-child(8){top:.640625em;left:.140625em;-webkit-animation-delay:-.7s;-moz-animation-delay:-.7s;-o-animation-delay:-.7s;-ms-animation-delay:-.7s;animation-delay:-.7s}.Spinner-inner:nth-child(9){top:.78125em;left:.28125em;-webkit-animation-delay:-.8s;-moz-animation-delay:-.8s;-o-animation-delay:-.8s;-ms-animation-delay:-.8s;animation-delay:-.8s}.Spinner-inner:nth-child(10){top:.828125em;left:.453125em;-webkit-animation-delay:-.9s;-moz-animation-delay:-.9s;-o-animation-delay:-.9s;-ms-animation-delay:-.9s;animation-delay:-.9s}.Spinner-inner:nth-child(11){top:.78125em;left:.640625em;-webkit-animation-delay:-1s;-moz-animation-delay:-1s;-o-animation-delay:-1s;-ms-animation-delay:-1s;animation-delay:-1s}.Spinner-inner:nth-child(12){top:.640625em;left:.78125em;-webkit-animation-delay:-1.1s;-moz-animation-delay:-1.1s;-o-animation-delay:-1.1s;-ms-animation-delay:-1.1s;animation-delay:-1.1s}@-moz-keyframes Spinner-animation{0%,20%,80%,to{-webkit-transform:scale(1);-moz-transform:scale(1);-o-transform:scale(1);-ms-transform:scale(1);transform:scale(1);opacity:0;-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";filter:alpha(opacity=0)}50%{-webkit-transform:scale(1.5);-moz-transform:scale(1.5);-o-transform:scale(1.5);-ms-transform:scale(1.5);transform:scale(1.5);opacity:1;-ms-filter:none;filter:none}}@-webkit-keyframes Spinner-animation{0%,20%,80%,to{-webkit-transform:scale(1);-moz-transform:scale(1);-o-transform:scale(1);-ms-transform:scale(1);transform:scale(1);opacity:0;-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";filter:alpha(opacity=0)}50%{-webkit-transform:scale(1.5);-moz-transform:scale(1.5);-o-transform:scale(1.5);-ms-transform:scale(1.5);transform:scale(1.5);opacity:1;-ms-filter:none;filter:none}}@-o-keyframes Spinner-animation{0%,20%,80%,to{-webkit-transform:scale(1);-moz-transform:scale(1);-o-transform:scale(1);-ms-transform:scale(1);transform:scale(1);opacity:0;-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";filter:alpha(opacity=0)}50%{-webkit-transform:scale(1.5);-moz-transform:scale(1.5);-o-transform:scale(1.5);-ms-transform:scale(1.5);transform:scale(1.5);opacity:1;-ms-filter:none;filter:none}}@keyframes Spinner-animation{0%,20%,80%,to{-webkit-transform:scale(1);-moz-transform:scale(1);-o-transform:scale(1);-ms-transform:scale(1);transform:scale(1);opacity:0;-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";filter:alpha(opacity=0)}50%{-webkit-transform:scale(1.5);-moz-transform:scale(1.5);-o-transform:scale(1.5);-ms-transform:scale(1.5);transform:scale(1.5);opacity:1;-ms-filter:none;filter:none}}
+	</style>
+	<script type="text/javascript">
+		var Widthparam = <? echo (isset($_REQUEST["screen_width"]) ? $_REQUEST["screen_width"] : "0"); ?>;
+		var Heightparam = <? echo (isset($_REQUEST["screen_height"]) ? $_REQUEST["screen_height"] : "0"); ?>;
+		var UserParam = <? echo (isset($_REQUEST["user"]) ? $_REQUEST["user"] : "0"); ?>;
+		var CampaignIdParam = '<? echo (isset($_REQUEST["campaignid"]) ? $_REQUEST["campaignid"] : "0"); ?>';
+		var KeywordsParams = '<? echo (isset($keywords) ? $keywords : ""); ?>';
+		var RecipeParams = '<? echo (isset($recipe_type) ? $recipe_type : "1"); ?>';
+		var MAIN_DOMAIN ='<? echo MAIN_DOMAIN; ?>';
+	</script>
+	<script type="text/javascript" src="includes/bexi_ad_selector.js"></script>
+</head>
+<body>
+<div class="main_selector">
+	<!-- Next and previous buttons -->
+	<a class="selector_prev hidden" onclick="plusSlides(1)"></a>
+	<a class="selector_next" onclick="plusSlides(-1)"></a>
+	<div class="bexi_sliders" id="modu_sliders" style="width: 95%; margin-left: 5%; position: relative; height: 100%; margin-right: 0%; overflow: hidden;" >
+	</div>
+</div>
+</body>
+</html>
---
+++ generator/includes/bexi_ad_selector.js
@@ -0,0 +1,353 @@
+var slideIndex = 1;
+var sliderSize = .40;
+var myWidth = 0;
+var myHeight = 0 ;
+var FrameSel = "";
+
+window.onmessage = function(e){
+  console.log("onmessage");
+  data = e.data.split("|");
+  console.log(e.data);
+  if (data[0] == 'SelectProject') {
+    looser = "";
+    $(".project_active").each(function(){
+      if ($(this).attr("modu-id") != data[1]){
+        looser = $(this).attr("modu-id");
+        $(".FrameSelected").removeClass("FrameSelected");
+      }
+    });
+    FrameSel = $.escapeSelector("frame-"+data[1]);
+    console.log("frame to sel ","#" + FrameSel);
+    $("#" + FrameSel).addClass("FrameSelected");
+    console.log(data[0] + '|' + data[1] + '|' + looser);
+    if (window.top != window.self) {
+      window.top.postMessage(data[0] + '|' + data[1] + '|' + looser, '*')
+    }
+
+  }
+};
+
+function uniqId() {
+  return Math.round(new Date().getTime() + (Math.random() * 100));
+}
+
+
+function GetWidthScreen()
+{
+  myWidth = 0;
+  if (window !== top) {
+  	myWidth = window.innerWidth ;
+  }else{
+  	myWidth = $(window).width();
+  }
+
+  if (myWidth==0)
+  {
+  	myWidth = window.screen.width;
+  }
+
+  if (myWidth==0)
+  {
+  		myWidth = Widthparam;
+  }
+  console.log("Widthparam");
+  console.log(Widthparam);
+  console.log("myWidth ");
+  console.log(myWidth);
+  return myWidth;
+};
+
+function GetHeightScreen()
+{
+  myHeight = 0;
+  if (window !== top) {
+    myHeight = window.innerHeight ;
+  }else{
+    myHeight = $(window).height();
+  }
+
+  if (myWidth==0)
+  {
+    myHeight = window.screen.height;
+  }
+
+  if (myHeight==0)
+  {
+      myHeight = Heightparam;
+  }
+  console.log("Heightparam");
+  console.log(Heightparam);
+  console.log("myHeight ");
+  console.log(myHeight);
+  return myHeight;
+};
+
+function frameload(ID){
+  $("#load-"+ID).remove();
+ }
+
+
+function AddNewProject()
+    {
+      //Revisamos si se han agregado menos de 2 sliders y si se han agregado menos de 2
+      //los marcamos como project_active para que se active el boton de seleccionar al dar click
+      console.log("numeros de Sliders: ",$("#modu_sliders .mySlides").length );
+      var ClassActive = "";
+      if ($("#modu_sliders .mySlides").length<2)
+      {
+            ClassActive = "project_active";
+      }else{
+          ClassActive = "project_inactive";
+      }
+      var uId=uniqId();
+      var newDiv = $(document.createElement('div'));
+      newDiv.attr("class","justify-content-center align-items-center text-center");
+      newDiv.attr("id","load-"+uId);
+      newDiv.css("width",GetWidthScreen()+"px");
+      newDiv.css("height",GetHeightScreen()+"px");
+      newDiv.css("padding-top",GetHeightScreen()+"px");
+      newDiv.css("position","absolute");
+      newDiv.css("z-index","100");
+      newDiv.html(
+        '<div class="Spinner"><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div></div>'
+        );
+      $("#modu_sliders").append('<div class="thumbnail-container mySlides"><div id="'+uId+'" class="thumbnail"></div></div>');
+      $.ajax({
+                url: 'adgenerator.php',
+                data: {"cmd" : "CreateAd", "user" : UserParam, "keywords" : KeywordsParams,"campaign_id": CampaignIdParam, "recipe":RecipeParams},
+                dataType: "json",
+                type: 'POST',
+                beforeSend: function(){
+                  // Show image container
+                  $("#"+uId).append(newDiv);
+                 },
+                 complete:function(data){
+                  // Hide image container
+                  //newDiv.remove();
+                 }
+            }).done(function( data, textStatus, jqXHR ) {
+              $("#"+uId).append('<iframe onload="frameload('+uId+')" id="frame-'+ data.codeid +'" class="' + ClassActive +  '" src="http://generator.'+MAIN_DOMAIN+'/adgenerator.php?cmd=selector&user=' + UserParam + '&codeid=' + data.codeid + '&campaignid=' + CampaignIdParam + '" frameborder="0" modu-id="' + data.codeid + '"></iframe>');
+              console.log($(".bexi_sliders .mySlides").length);
+               if ($(".bexi_sliders .mySlides").length<=4)
+               {
+                 var n=1;
+                 $(".bexi_sliders .mySlides").each(function() {
+                   if (n<=4)
+                   {
+                     console.log("sliders");
+                     var lpos =(n-1) * sliderSize * GetWidthScreen();
+                     console.log("left:" + lpos);
+                     $(this).css({ 'top': 0, 'left': lpos });
+                   }
+                     n = n +1;
+                 });
+              }
+           if ( console && console.log ) {
+             console.log("data");
+             console.log(data);
+               console.log( "La solicitud se ha completado correctamente." );
+           }
+       })
+       .fail(function( jqXHR, textStatus, errorThrown ) {
+           if ( console && console.log ) {
+               console.log( "La solicitud a fallado: " +  textStatus);
+           }
+      });
+    }
+
+ $(function() {
+//Set Variables 
+   document.documentElement.style.setProperty('--thumbnail-width', GetWidthScreen()+"px");
+   //document.documentElement.style.setProperty('--thumbnail-height', (GetHeightScreen() * 2.5) + "px");
+/*function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  //var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      //slides[i].style.display = "none";
+  }
+ /* for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  //dots[slideIndex-1].className += " active";
+} */
+
+  AddNewProject();
+  AddNewProject();
+  AddNewProject();
+  AddNewProject();
+  console.log("slider");
+
+  var n=1;
+     $(".bexi_sliders .mySlides").each(function() {
+       if (n<=4)
+       {
+         console.log("slider");
+         var lpos =(n-1) * sliderSize * GetWidthScreen();
+         console.log("left:" + lpos);
+         $(this).css({ 'top': 0, 'left': lpos });
+       }
+         n = n +1;
+     });
+
+});
+
+$( document ).ready(function() {
+    console.log("ready");
+});
+
+function frameload(ID){
+  $("#load-"+ID).remove();
+ }
+
+ function plusSlides(n)
+ {
+ 	//console.log("plusSlides(n)");
+ 	//console.log(n);
+ 	//console.log("slideIndex=" + slideIndex);
+ 	if (n<0)
+ 	{
+ 		var nPages = $(".bexi_sliders .mySlides").length;
+
+ 		
+ 		//console.log("nPages=" + nPages);
+
+ 		if ((slideIndex - nPages) <= 3)
+ 		{
+      $(".selector_prev").removeClass("hidden");
+      $(".selector_prev").addClass("visible");
+ 			var npos = 1;
+ 			var slider = 0;
+       var uId=uniqId();
+       var newDiv = $(document.createElement('div'));
+       newDiv.attr("class","justify-content-center align-items-center text-center");
+       newDiv.attr("id","load-"+uId);
+       newDiv.css("width",GetWidthScreen()+"px");
+       newDiv.css("height",GetHeightScreen()+"px");
+       newDiv.css("padding-top",GetHeightScreen()+"px");
+       newDiv.css("position","absolute");
+       newDiv.css("z-index","100");
+       newDiv.html(
+         '<div class="Spinner"><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div><div class="Spinner-inner"></div></div>'
+         );
+         $("#modu_sliders").append('<div class="thumbnail-container mySlides"><div id="'+uId+'" class="thumbnail"></div></div>');
+
+         //<div class="thumbnail-container mySlides" style="top:0px; left: 9999px"><div class="thumbnail"> 
+        $.ajax({
+                url: 'adgenerator.php',
+                data: {"cmd" : "CreateAd", "user" : UserParam, "keywords" : KeywordsParams,"campaign_id": CampaignIdParam, "recipe":RecipeParams},
+                dataType: "json",
+                type: 'POST',
+                async : true,
+                beforeSend: function(){
+
+                 },
+                 complete:function(data){
+                  // Hide image container
+                  //newDiv.remove();
+                 }
+            }).done(function( data, textStatus, jqXHR ) {
+                // Show image container
+                $("#"+uId).append(newDiv);
+                $("#"+uId).append('<iframe onload="frameload('+uId+')" id="frame-'+ data.codeid +'" class="' + ClassActive +  '" src="http://generator.'+MAIN_DOMAIN+'/adgenerator.php?cmd=selector&user=' + UserParam + '&codeid=' + data.codeid + '&campaignid=' + CampaignIdParam + '" frameborder="0" modu-id="' + data.codeid + '"></iframe>');
+              })
+       .fail(function( jqXHR, textStatus, errorThrown ) {
+           if ( console && console.log ) {
+               console.log( "La solicitud a fallado: " +  textStatus);
+           }
+      });
+    
+ 			$(".bexi_sliders .mySlides").each(function() {
+ 				/*if (npos == slideIndex)
+ 				{
+ 					 $(this).animate({
+					    left: '-9999px',
+					    opacity: '0.5'
+					  }, 800);
+ 				}*/
+
+ 				//console.log("npos=" + npos);
+
+ 				var pos = npos - slideIndex - 1;
+
+ 				//console.log("pos=" + pos);
+ 				//console.log("myWidth = " + myWidth);
+ 				//console.log("left=" + ( pos * sliderSize * myWidth));
+         //console.log("nframe - " + (npos-slideIndex));
+ 				//if ( pos>=0  && pos <= 3)
+ 				//{
+ 					$(this).animate({
+					    left: pos * sliderSize * myWidth,
+					   // opacity: '1'
+					 }, 800);
+ 				//}
+        if ((npos-slideIndex)>=1 && (npos-slideIndex)<=2)
+        {
+           $("iframe", this).addClass("project_active");
+           $("iframe", this).removeClass("project_inactive");
+        }else{
+           $("iframe", this).removeClass("project_active");
+           $("iframe", this).addClass("project_inactive");
+        }
+ 				npos = npos + 1;
+ 			});
+ 		}
+ 		
+ 		slideIndex = slideIndex + 1;
+ 	}
+
+ 	//console.log("n=" + n);
+ 	if (n>0 && slideIndex > 1)
+ 	{
+ 		//console.log("prev");
+ 		var nPages = $(".bexi_sliders .mySlides").length;
+ 		
+ 			/*$(".main_selector").append('<div class="thumbnail-container mySlides" style="top:0px; left: 9999px"><div class="thumbnail"> <iframe src="http://generator.bexi.co/generator.localhost.php" frameborder="0" onload="this.style.opacity = 1"></iframe></div>');
+ 			*/
+ 		var npos = 1;
+ 		var slider = 0;
+ 		$(".bexi_sliders .mySlides").each(function() {
+ 				
+ 			var pos = npos - slideIndex + 1 ;
+ 			//console.log("Prev pos==" + pos);
+ 			//console.log("Prev left=" + (pos * sliderSize * myWidth));
+ 			$(this).animate({
+				left: pos * sliderSize * myWidth,
+				//opacity: '1'
+			}, 800);
+
+       //console.log("nframe - " + (npos-slideIndex));
+       //console.log("left: ", npos-slideIndex);
+
+       /****************************************************
+       Activamos los frames que se movera a la pantalla principal el frame -1 que se volvera 0 y el 0 que se volvera 1
+       *****************************************************/
+       if ((npos-slideIndex)>=-1 && (npos-slideIndex)<=0)
+        {
+           //console.log("project_active : ", npos-slideIndex);
+           $("iframe",this).addClass("project_active");
+           $("iframe", this).removeClass("project_inactive");
+        }else{
+          //console.log("project_inactive : ", npos-slideIndex);
+          $("iframe",this).removeClass("project_active");
+          $("iframe", this).addClass("project_inactive");
+        }
+
+ 			npos = npos + 1;
+ 		});
+ 		slideIndex = slideIndex - 1;
+   }
+   if(slideIndex===1)
+   {
+     $(".selector_prev").addClass("hidden");
+     $(".selector_prev").removeClass("visible");
+   }
+ }
+
+
+
+
+
