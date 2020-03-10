833c9a1275ee0e3cf20d65e7ead0526c8b4a0496
Manuel Cuevas <manuel@bexi.io>
2020-03-10 11:04:32 -0700

    debug opt

--- generator/api/v2/campaigns.php
+++ generator/api/v2/campaigns.php
@@ -48,6 +48,7 @@ function CreateCampaign($connDyn, $userid, $cname, $cgoal, $industry, $colors, $
 
 	$resIns=Insert("modu_campaigns",$Data, false);
 
+	print_r($ProjectsTypes); 
 	foreach ($ProjectsTypes as $ptype)
 	{
 		echo "CreateNewProject".$cId." - ".$cname." - ".$ptype." - ".$ptype;
