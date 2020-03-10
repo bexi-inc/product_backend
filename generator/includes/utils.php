d3c60e1f14249d8b5401a3620b8f4979429c29a3
Adryx46 <adrian@bexi.io>
2020-03-10 14:04:20 -0700

    Update utils.php

--- generator/includes/utils.php
+++ generator/includes/utils.php
@@ -236,7 +236,6 @@ function setImages($contenido,$keywords = ""){
 	            'h'        => $imgdata[2],
 	            'query'    => $keywords
 	    ];
-	    print_r($filters);
 	    try
 	    {
 	   	 	$data = Crew\Unsplash\Photo::random($filters);
@@ -247,7 +246,7 @@ function setImages($contenido,$keywords = ""){
 		            'w'        => $imgdata[1],
 		            'h'        => $imgdata[2]
 		    ];
-
+			print_r($filters);
 		    try
 	    	{
 		    	$data = Crew\Unsplash\Photo::random($filters);
