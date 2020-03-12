38361a6065833ff523156ae7fbd786e8d9907b7b
Adryx46 <adrian@bexi.io>
2020-03-12 12:40:11 -0700

    Update bexi_ad_selector.js

--- generator/includes/bexi_ad_selector.js
+++ generator/includes/bexi_ad_selector.js
@@ -244,7 +244,7 @@ function AddNewProject()
 
       var size=$(".thumbnail-container").width()+20;
       console.log($("#pre-thumbnail").css("top"));
-      if(((slideIndex-1)*size)+"px"== $("#pre-thumbnail").css("top")){
+      if(((slideIndex-1)*size)+"px"== $("#pre-thumbnail").css("top")||$("#pre-thumbnail").css("top")=="auto"){
         $("#pre-thumbnail").animate({top: slideIndex*size},{
           step: function(now,fx) {
             $(this).css('-webkit-transform',"translateX(-"+(now)+"px)");
