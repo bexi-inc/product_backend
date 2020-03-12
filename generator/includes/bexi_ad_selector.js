f43fc7c61d24d020a05bfe664ec2a5cfcb82e0f9
Adryx46 <adrian@bexi.io>
2020-03-12 12:25:05 -0700

    Update bexi_ad_selector.js

--- generator/includes/bexi_ad_selector.js
+++ generator/includes/bexi_ad_selector.js
@@ -75,7 +75,9 @@ function frameload(ID){
 
  $(window).on('resize', function(){
   document.documentElement.style.setProperty('--zoom-factor', GetWidthScreen()/2742.8571);
+  console.log(slideIndex);
   var size=$(".thumbnail-container").width()+20;
+  console.log(size);
   $("#pre-thumbnail").css('-webkit-transform',"translateX(-"+(slideIndex*size)+"px)");
   $("#pre-thumbnail").css('-moz-transform',"translateX(-"+(slideIndex*size)+"px)");
   $("#pre-thumbnail").css('transform',"translateX(-"+(slideIndex*size)+"px)");
