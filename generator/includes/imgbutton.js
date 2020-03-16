bfce501464563fa4a53fc239cfb632d3d6a2d7a5
Adryx46 <adrian@bexi.io>
2020-03-16 12:46:32 -0700

    Update imgbutton.js

--- generator/includes/imgbutton.js
+++ generator/includes/imgbutton.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     var node = document.getElementById('maindiv');
     domtoimage.toPng(node)
-    .then(function (dataUrl) {
-        var img = new Image();
-        img.src = dataUrl;
-        document.body.appendChild(img);
+    .then(function (blob) {
+        window.saveAs(blob, 'my-node.png');
     })
+    // .then(function (dataUrl) {
+    //     var img = new Image();
+    //     img.src = dataUrl;
+    //     document.body.appendChild(img);
+    // })
     .catch(function (error) {
         console.error('oops, something went wrong!', error);
     });
