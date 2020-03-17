a44024de65be519d704fd9a3352367270ed9e235
Adryx46 <adrian@bexi.io>
2020-03-17 11:33:50 -0700

    Update imgbutton.js

--- generator/includes/imgbutton.js
+++ generator/includes/imgbutton.js
@@ -11,18 +11,26 @@ $(document).ready(function() {
     // useCORS: true
     // });
 
-    
+
     html2canvas(document.querySelector("#maindiv") ,{allowTaint: false, useCORS: true,backgroundColor:null}).then(canvas => {
-        canvas.width=300;
-        canvas.height=250;
-        document.body.appendChild(canvas)
+        var extra_canvas = document.createElement("canvas");
+        extra_canvas.setAttribute('width',300);
+        extra_canvas.setAttribute('height',250);
+        var ctx = extra_canvas.getContext('2d');
+        ctx.drawImage(canvas,0,0,canvas.width, canvas.height,0,0,300,250);
+        var dataURL = extra_canvas.toDataURL();
+        var img = $(document.createElement('img'));
+        img.attr('src', dataURL);
+        // insert the thumbnail at the top of the page
+        $('body').appendChild(img);
+
         var link = document.createElement('a');
         link.download = 'my-image-name.jpeg';
-        link.href = canvas.toDataURL("image/png");
+        link.href = dataURL;
         link.click();
     });
-    
-    
+
+
     // var node = document.getElementById('maindiv');
     // domtoimage.toPng(node)
     // .then(function (dataUrl) {
