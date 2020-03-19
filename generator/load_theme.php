246b469b10d239a96dfad62e1b69429163857136
Manuel Cuevas <manuel@bexi.io>
2020-03-19 08:30:52 -0700

    debug

--- generator/load_theme.php
+++ generator/load_theme.php
@@ -49,11 +49,12 @@ if (count($result["Items"])>0)
 
     $result = $dynamodb->query($params);
 
-    //print_r($result);
+    print_r($result);
 
     if (count($result["Items"])>0)
     {
         $colors = $marshaler->unmarshalValue($result["Items"][0]["colors"]);
+        print_r($colors);
         if (count($colors)>0)
         {
             $varcol="";
