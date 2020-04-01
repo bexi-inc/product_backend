ee04003b68f41c6cab6cfb97e580ee59fcb3d624
Adryx46 <adrian@bexi.io>
2020-04-01 12:39:37 -0700

    Update ad_selector-service.php

--- generator/internal/ad_selector-service.php
+++ generator/internal/ad_selector-service.php
@@ -32,7 +32,7 @@ Crew\Unsplash\HttpClient::init([
 ?><!DOCTYPE html>
 <html>
 <head>
-	<script src="../includes/jquery-3.4.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 	<style type="text/css">
 			/* Variables */
 
