a2f7bc2a1bca7a77d2de31cb2e732831df15245a
Adryx46 <adrian@bexi.io>
2020-04-02 12:07:37 -0700

    Update ad_editor-service.js

--- generator/internal/ad_editor-service.js
+++ generator/internal/ad_editor-service.js
@@ -1465,8 +1465,8 @@ function initialize_editors_text(){
 
       // log the detail property to the console
       console.log("doit");
-      $(".bexi_title").css("white-space","normal !important");
+      $(".bexi_title").css("white-space","normal");
     });
-    
+
     fitty('.bexi_button');
 }
