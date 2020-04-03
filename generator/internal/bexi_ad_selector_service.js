df4e3433fefa3d07c8bde0828a6e87338ba36ab8
Adryx46 <adrian@bexi.io>
2020-04-03 15:40:08 -0700

    Update bexi_ad_selector_service.js

--- generator/internal/bexi_ad_selector_service.js
+++ generator/internal/bexi_ad_selector_service.js
@@ -24,7 +24,7 @@ window.onmessage = function(e){
 };
 
 function uniqId() {
-  return Math.round(new Date().getTime() + (Math.random() * 100));
+  return Math.floor(new Date().getTime() + (Math.random() * 100));
 }
 
 
