6e09fde65f4de0a7b366eb0b24fe97a777ed7e43
Adryx46 <adrian@bexi.io>
2020-04-07 13:39:24 -0600

    Update ad_editor-service.js

--- generator/internal/ad_editor-service.js
+++ generator/internal/ad_editor-service.js
@@ -998,7 +998,7 @@ function createimg(){
   });
 
 
-    html2canvas(document.querySelector(".bexi_module_ad") ,{allowTaint: false,dpi: 300,useCORS: true,backgroundColor:null,width:1200,height:628}).then(canvas => {
+    html2canvas(document.querySelector(".bexi_module_ad") ,{allowTaint: false,scale:2,dpi: 300,useCORS: true,backgroundColor:null,width:1200,height:628}).then(canvas => {
       var dataURL = canvas.toDataURL('image/jpeg', 1.0);
       var pid=$("#codeId").val();
 
