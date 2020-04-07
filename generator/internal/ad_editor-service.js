33f317fd46df23d00fa5e4ff923b8c9e638946d0
Adryx46 <adrian@bexi.io>
2020-04-07 14:10:02 -0600

    Update ad_editor-service.js

--- generator/internal/ad_editor-service.js
+++ generator/internal/ad_editor-service.js
@@ -1003,9 +1003,10 @@ function createimg(){
 
       /********** EXTRA CANVAS **********/
       var extra_canvas = document.createElement("canvas");
-      extra_canvas.setAttribute('width',1200*3);
-      extra_canvas.setAttribute('height',628*3);
+      extra_canvas.setAttribute('width',1200);
+      extra_canvas.setAttribute('height',628);
       var ctx = extra_canvas.getContext('2d');
+      ctx.scale(1/3,1/3);
       ctx.drawImage(canvas,0,0,canvas.width, canvas.height,0,0,1200*3,628*3);
       var dataURL = extra_canvas.toDataURL("image/jpeg",1.0);
 
