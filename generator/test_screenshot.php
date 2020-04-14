ce3e32442c327d57ed80d0a7729433ee13ed487b
Manuel Cuevas <manuel@bexi.io>
2020-04-14 17:32:07 -0600

    Fix Error

--- generator/test_screenshot.php
+++ generator/test_screenshot.php
@@ -5,5 +5,8 @@ $Web = new Chrome();
 $Web->Navigate("https://stackoverflow.com/");
 
 $Web->SaveScreenShot("/var/www/uploads.getmodu.com/public_html/102/
-");
+thumbnail.png");
+
+echo "<IMG src='/var/www/uploads.getmodu.com/public_html/102/
+thumbnail.png' />"
 ?>
