8ccaf9e49609a3e9be4d75295895915fe8b6c6be
Manuel Cuevas <manuel@bexi.io>
2020-04-14 17:33:30 -0600

    add param to test_screenshot.php

--- generator/test_screenshot.php
+++ generator/test_screenshot.php
@@ -2,7 +2,13 @@
 include("includes/Chrome.php");
 
 $Web = new Chrome();
-$Web->Navigate("https://stackoverflow.com/");
+if ($_REQUEST["web"])
+{
+	$Web->Navigate($_REQUEST["web"]);
+}else{
+	$Web->Navigate("https://stackoverflow.com/");
+}
+
 
 $Web->SaveScreenShot("/var/www/uploads.getmodu.com/public_html/102/
 thumbnail.png");
