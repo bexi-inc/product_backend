020ea6f130666d8a736a4a361bc49652ca2a49ea
chrisgarcia96 <60152967+chrisgarcia96@users.noreply.github.com>
2020-04-14 09:32:14 -0600

    Update ad_editor.js

--- generator/includes/ad_editor.js
+++ generator/includes/ad_editor.js
@@ -440,11 +440,15 @@ $(document).ready(function() {
             '</div>'+
           '</div>'+
           '<div id="tab-3">'+
+          '<div class="row">'+
+            '<div class="w-50">'+
             '<div class="input-group mb-3">'+
               '<input id="inptextsearch'+num+'" type="text" class="form-control" placeholder="Keywords..."  aria-describedby="button-addon2">'+
               '<div class="input-group-append">'+
                 '<button class="btn ui-button ui-corner-all ui-widget" type="button" onclick="set_pagination2(\''+num+'\','+1+');" id="button-addon2">Search</button>'+
               '</div>'+
+              '</div>'+
+            '</div>'+
             '</div>'+
             '<div id="cont_unspl'+num+'">'+
             '</div>'+
@@ -508,12 +512,16 @@ FroalaEditor.RegisterCommand('unsplash_manager', {
     newDiv.css("width", "auto");
     newDiv.css("overflow", "auto");
     newDiv.html(
+    '<div class="row m-0">'+
+    '<div class="w-50">'+
     '<div class="input-group mb-3">'+
     '<input id="inptext'+ID+'" type="text" class="form-control" placeholder="Keywords..."  aria-describedby="button-addon2">'+
     '<div class="input-group-append">'+
       '<button class="btn btn-outline-primary" type="button" onclick="set_pagination(\''+ID+'\','+1+');" id="button-addon2">Search</button>'+
     '</div>'+
   '</div>'+
+  '</div>'+
+    '</div>'+
   '<div id="cont_unspl'+ID+'">'+
   '</div>'+
   '<div id="cont_pag'+ID+'" class="pagination">'+
