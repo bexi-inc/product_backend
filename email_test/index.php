0ddfe38898b52d9d4b88ca21a42d510bcb937730
Manuel Cuevas <manuel@bexi.io>
2020-05-06 16:27:44 -0600

    add config.php and connect to Dynamodb

--- email_test/index.php
+++ email_test/index.php
@@ -1,6 +1,21 @@
 <?
+require "config.php";
+require 'vendor/autoload.php';
+
+date_default_timezone_set('UTC');
+
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Aws\DynamoDb\Marshaler;
+
+$credentials = new Aws\Credentials\Credentials(AWS_KEY, AWS_PASS);
+
+$sdk = new Aws\Sdk([
+    'region'   => AWS_REGION,
+    'version'  => 'latest',
+    'credentials' => $credentials
+]);
+
 
 echo "email_test";
 
 ?>
-
