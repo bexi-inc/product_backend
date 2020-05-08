d090e8e2f0b5fb51ecb412009286523a1da76cf5
Manuel Cuevas <manuel@bexi.io>
2020-05-08 13:27:48 -0600

    Fix Error

---
+++ email_test/partials/space.php
@@ -0,0 +1,12 @@
+<table
+border="0"
+              cellpadding="0"
+              cellspacing="0"
+              width="100%"
+              role="presentation"
+>
+<tr>
+  <td colspan="0" height="26"></td>
+</tr>
+
+</table>
